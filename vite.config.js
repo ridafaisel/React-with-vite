1e56b97d8d6a4d8f58811222037dabce251ffd1b
Redahalojayen <136948571+Redahalojayen@users.noreply.github.com>
2025-12-04 19:41:48 +0300

    remove base

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),]
- ,
-  base: './',  // This is important for Netlify
-  
+  plugins: [react()],
 })
