a8f9a1e975b79b331e540b3d910e3bc1067f0c54
Redahalojayen <136948571+Redahalojayen@users.noreply.github.com>
2025-12-04 19:02:32 +0300

    starter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Learn React with chaicode.com</h1>
+      <p>This is a subb heading</p>
+    </>
+  )
+}
+
+export default App
